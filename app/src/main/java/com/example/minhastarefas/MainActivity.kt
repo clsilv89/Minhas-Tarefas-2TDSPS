e65de8806c81e6f3d07b821c174fa057849ab766
Caio <caio.silva@ab-inbev.com>
2024-04-19 16:54:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/minhastarefas/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.minhastarefas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.minhastarefas.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        abrirTelaListaTarefas()
+    }
+
+    private fun abrirTelaListaTarefas() {
+        val listaTarefasFragment = ListaTarefasFragment.newInstance({
+            abrirTelaCriarTarefas()
+        }, "")
+
+        supportFragmentManager.beginTransaction().replace(
+            binding.frameLayout.id,
+            listaTarefasFragment
+        ).commit()
+    }
+
+    private fun abrirTelaCriarTarefas() {
+        val criaTarefasFragment = CriaTarefasFragment.newInstance({
+            val tarefa = Tarefa(
+                descricao = it,
+                completa = false
+            )
+            println(tarefa)
+        }, "")
+
+        supportFragmentManager.beginTransaction().replace(
+            binding.frameLayout.id,
+            criaTarefasFragment
+        ).commit()
+    }
+
+   
+}
